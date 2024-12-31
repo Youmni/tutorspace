1706e32637e81628149c3d6dcfdb4af640bc874f
Youmni <youmni.malha@gmail.com>
2025-01-01 00:06:19 +0100

    Fix tailwind css issue in admin sidebar

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -7,15 +7,16 @@
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet">
     @vite('resources/css/app.css')
 </head>
-<body class="flex flex-col md:flex-row min-h-screen max-w-screen bg-gray-100 text-gray-900">
-    <div class="flex flex-col h-auto md:h-screen bg-navy-800 text-white w-full md:w-64 shadow-lg">
+<body class="flex flex-col md:flex-row min-h-screen bg-gray-100 text-gray-900">
+
+    <div class="flex flex-col bg-navy-800 text-white w-full md:w-64 shadow-lg">
         <div class="flex items-center justify-between md:justify-center h-20 border-b border-navy-700 px-4 md:px-0">
             <h1 class="text-2xl font-semibold">Admin</h1>
             <button id="menu-toggle" class="md:hidden">
                 <span class="material-icons">menu</span>
             </button>
         </div>
-        <nav id="sidebar" class="flex grow mt-4 md:mt-0 md:block hidden">
+        <nav id="sidebar" class="flex-grow mt-4 md:mt-0 md:block hidden">
             <ul class="flex flex-col gap-4 px-4">
                 <li>
                     <a href="{{ route('admin.home.index') }}" class="flex items-center gap-4 p-4 rounded-md hover:text-black hover:bg-white transition-colors text-lg w-full {{ request()->routeIs('admin.home.index') ? 'bg-white text-black' : '' }}">
@@ -45,7 +46,7 @@
         </nav>
     </div>
 
-    <main class="flex-grow p-2 md:p-2">
+    <main class="flex-grow p-4 md:p-6 overflow-auto">
         <div class="bg-white p-6 rounded-lg shadow-md">
             @yield('content')
         </div>
